5919ad1971bd3aeddbcc6655901998de53b4c700
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-03 16:46:54 -0300

    Push queue

--- app/routes.php
+++ app/routes.php
@@ -3,35 +3,62 @@
 /**
  * Push queues
  */
-Route::post('/queue/canaport', function(){
+Route::post('/queue/canaport', function ()
+{
     return Queue::marshal();
+});
+
+Route::get('/testqueue', function ()
+{
+
+    $user_array = array(
+        'email' => 'trevor.sawler@gmail.com',
+        'name'  => 'Trevor'
+    );
+
+    $data = array(
+        'users_name'  => $user_array['name'],
+        'the_message' => 'Hello, world',
+        'email'       => 'some email'
+    );
+
+    Mail::queue('emails.cleanup_email', $data, function ($message) use ($user_array)
+    {
+        $message->from('donotreply@canaportlng.com', 'Do not reply');
+        $message->to('trevor.sawler@gmail.com')->subject('Test of email');
+    });
+
+    return "sent";
 });
 
 
 /**
  * Home Page
  */
-Route::any('/','PageController@showHome');
-Route::any('/home','PageController@showHome');
+Route::any('/', 'PageController@showHome');
+Route::any('/home', 'PageController@showHome');
 
-Route::get('/redheadcleanup', function(){
+Route::get('/redheadcleanup', function ()
+{
     return View::make('redhead-register');
 });
 
-Route::post('/redheadcleanup', function(){
+Route::post('/redheadcleanup', function ()
+{
 
     $user = array(
-        'email'=>Input::get('email'),
-        'name'=>Input::get('name')
+        'email' => Input::get('email'),
+        'name'  => Input::get('name')
     );
 
     $data = array(
         'users_name'  => $user['name'],
-        'the_message'=>Input::get('message'),
-        'email'=>Input::get('email')
+        'the_message' => Input::get('message'),
+        'email'       => Input::get('email')
     );
 
-    Mail::later(5,'emails.cleanup_email', $data, function($message) use ($user) {
+    Mail::later(5, 'emails.cleanup_email', $data, function ($message) use ($user)
+    {
         $message->from('donotreply@canaportlng.com', 'Do not reply');
         $message->to(Config::get('app.contact_email'))->subject('Redhead Cleanup Registration');
     });
@@ -42,32 +69,31 @@ Route::post('/redheadcleanup', function(){
 /*
  * Email consent routes
  */
-Route::get('/emailconsent','EmailConsentController@getShowform');
-Route::post('/emailconsent','EmailConsentController@postShowform');
+Route::get('/emailconsent', 'EmailConsentController@getShowform');
+Route::post('/emailconsent', 'EmailConsentController@postShowform');
 
 /**
  *
  * Mailing list routes
  */
-Route::get('/Signup+For+Updates','MailRecipientController@getJoinList');
-Route::post('/Signup+For+Updates','MailRecipientController@postJoinList');
+Route::get('/Signup+For+Updates', 'MailRecipientController@getJoinList');
+Route::post('/Signup+For+Updates', 'MailRecipientController@postJoinList');
 
 
 /**
  * TWitter feed Routes
  */
-Route::get('/twitter', function()
+Route::get('/twitter', function ()
 {
     return Twitter::getUserTimeline(array('screen_name' => 'canaportlng', 'count' => 20, 'format' => 'json'));
 });
-
 
 
 /**
  *
  * Proces routes
  */
-Route::get('/process', function()
+Route::get('/process', function ()
 {
     return View::make('pages.process');
 });
@@ -84,55 +110,54 @@ Route::post('/Contact+Us', 'ContactController@postContact');
  *
  * Minutes routes
  */
-Route::get('/minutes','MinutesController@showMinutes');
+Route::get('/minutes', 'MinutesController@showMinutes');
 
 
 /**
  *
  * newsletter routes
  */
-Route::get('/connections','NewsletterController@getNewsletters');
-
+Route::get('/connections', 'NewsletterController@getNewsletters');
 
 
 /**
  * Calendar
  */
-Route::get('/calendar','CalendarEventController@showEvents');
-Route::get('/calendar/addevent','CalendarEventController@showAddEvent');
-Route::post('/calendar/addevent','CalendarEventController@handleAddEvent');
-Route::get('/calendar/allevents','CalendarEventController@allEvents');
-Route::post('/calendar/allevents','CalendarEventController@postAllEvents');
-Route::get('/calendar/delete/{eventid}','CalendarEventController@handleDelete');
-Route::get('/calendar/editevent/{eventid}','CalendarEventController@editEvent');
-Route::post('/calendar/editevent/{eventid}','CalendarEventController@handleEditEvent');
+Route::get('/calendar', 'CalendarEventController@showEvents');
+Route::get('/calendar/addevent', 'CalendarEventController@showAddEvent');
+Route::post('/calendar/addevent', 'CalendarEventController@handleAddEvent');
+Route::get('/calendar/allevents', 'CalendarEventController@allEvents');
+Route::post('/calendar/allevents', 'CalendarEventController@postAllEvents');
+Route::get('/calendar/delete/{eventid}', 'CalendarEventController@handleDelete');
+Route::get('/calendar/editevent/{eventid}', 'CalendarEventController@editEvent');
+Route::post('/calendar/editevent/{eventid}', 'CalendarEventController@handleEditEvent');
 
 
 /**
  * Search site
  */
-Route::get('/search','SearchController@showSearchPage');
-Route::post('/search','SearchController@performSearch');
+Route::get('/search', 'SearchController@showSearchPage');
+Route::post('/search', 'SearchController@performSearch');
 
 
 /**
  * Community Involvement
  */
-Route::get('/Community+Involvement','InvolvementController@showPage');
-Route::post('/involvement/edit','InvolvementController@editInvolvement');
+Route::get('/Community+Involvement', 'InvolvementController@showPage');
+Route::post('/involvement/edit', 'InvolvementController@editInvolvement');
 
 /**
  * FAQs
  */
-Route::get('/faq','FaqController@showFaqPage');
-Route::post('/faq/edit','FaqController@editFaq');
+Route::get('/faq', 'FaqController@showFaqPage');
+Route::post('/faq/edit', 'FaqController@editFaq');
 
 
 /**
  * Misconceptions
  */
-Route::get('/misconceptions','MisconceptionsController@showMisconceptionsPage');
-Route::post('/misconceptions/edit','MisconceptionsController@editFaq');
+Route::get('/misconceptions', 'MisconceptionsController@showMisconceptionsPage');
+Route::post('/misconceptions/edit', 'MisconceptionsController@editFaq');
 
 
 /**
@@ -145,27 +170,25 @@ Route::get('/gallery', 'GalleryController@showFeed');
  * Blog (news) Routes
  */
 Route::controller('/post', 'BlogController');
-Route::post('/searchnews','SearchController@performBlogSearch');
+Route::post('/searchnews', 'SearchController@performBlogSearch');
 Route::get('/news/{year?}/{month?}', 'PostsController@index')->where(array('year' => '\d{4}', 'month' => '\d{2}'));
 Route::get('/news/{slug}', 'PostsController@view');
 Route::get('/news.rss', 'PostsController@rss');
-
 
 
 /**
  * User/account routes
  */
 Route::controller('/users', 'UserController');
-Route::get('/verifyaccount','UsersPendingController@validateUser');
+Route::get('/verifyaccount', 'UsersPendingController@validateUser');
 Route::controller('/password', 'RemindersController');
-
 
 
 /**
  *
  * Ajax routes
  */
-Route::controller('/ajax','AjaxController');
+Route::controller('/ajax', 'AjaxController');
 
 
 /**
@@ -177,14 +200,14 @@ Route::controller('/menu', 'MenuController');
 /**
  * Admin Routes
  */
-Route::controller('/admin','AdminController');
+Route::controller('/admin', 'AdminController');
 
 
 /**
  * Page Routes
  */
-Route::get('/Apply+For+Sponsorship','PageController@getApplyforsponorship');
-Route::post('/Apply+For+Sponsorship','PageController@postApplyforsponorship');
-Route::get('{pagename?}','PageController@showPage');
-Route::post('/page/edit','PageController@editPage');
+Route::get('/Apply+For+Sponsorship', 'PageController@getApplyforsponorship');
+Route::post('/Apply+For+Sponsorship', 'PageController@postApplyforsponorship');
+Route::get('{pagename?}', 'PageController@showPage');
+Route::post('/page/edit', 'PageController@editPage');
 
