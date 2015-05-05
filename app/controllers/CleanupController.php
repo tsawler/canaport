5ee37b8bafdbe8bb1bd68afa3c2bad8978d64672
Trevor Sawler <trevor.sawler@gmail.com>
2015-05-05 14:13:38 -0300

    Cleanup forms generalized

---
+++ app/controllers/CleanupController.php
@@ -0,0 +1,54 @@
+<?php
+
+class CleanupController extends BaseController {
+
+    public function __construct()
+    {
+        $this->beforeFilter('csrf', array('on' => 'post'));
+        $this->beforeFilter('auth', array('on' => 'get'));
+        $this->beforeFilter('auth', array('only' => 'post'));
+    }
+
+    public function getCleanup()
+    {
+        if (Auth::user()->access_level == 3) {
+            return View::make('users.dashboard.cleanup-choose');
+        } else {
+            return View::make('users.dashboard.dashboard');
+        }
+    }
+
+    public function getMarsh()
+    {
+        $page = Cleanup::find(1);
+
+        return View::make('users.dashboard.edit-cleanup')
+            ->with('page', $page);
+    }
+
+
+    public function postMarsh()
+    {
+        $page = Cleanup::find(1);
+        $page->title = Input::get('title');
+        $page->above = Input::get('above');
+        $page->below = Input::get('below');
+        if (Input::hasFile('image')) {
+            // get the file
+            $file = Input::file('image');
+            $destinationPath = base_path() . '/public/img/';
+            $filename = str_random(10) . "_" . $file->getClientOriginalName();
+
+            $upload_success = Input::file('image')->move($destinationPath, $filename);
+            $page->image = $filename;
+        }
+        $page->save();
+        return Redirect::to('/admin/cleanup/cleanup');
+    }
+
+    public function getRedhead()
+    {
+        return "redhead";
+    }
+
+}
